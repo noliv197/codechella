5c53c23cea23f70b8e174dcd34c2806afabcbb8e
natalia <>
2025-08-27 21:21:01 -0300

    update packages

--- src/components/Information/Description/index.tsx
+++ src/components/Information/Description/index.tsx
@@ -25,11 +25,6 @@ function Description(){
             id: 4,
             title:"Quais são os itens proibidos?",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        },
-        {
-            id: 5,
-            title:"Quais são os itens proibidos?",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         }
     ]
     return(
